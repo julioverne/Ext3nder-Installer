8428fdd866bcb9e0060a58b45173155bb5f80121
julioverne <jlio_verne@hotmail.com>
2018-07-07 16:43:34 -0300

    019

--- postinst-installer/main.mm
+++ postinst-installer/main.mm
@@ -1,9 +1,8 @@
 #import <sys/stat.h>
 #import <spawn.h>
+#import <dlfcn.h>
 #import <CommonCrypto/CommonCrypto.h>
 #import <Security/Security.h>
-
-OBJC_EXTERN CFStringRef MGCopyAnswer(CFStringRef key);
 
 #define info(asa ...) printf(asa)
 #include "libfragmentzip.h"
@@ -11,20 +10,48 @@ OBJC_EXTERN CFStringRef MGCopyAnswer(CFStringRef key);
 
 #define tmpDir "/tmp/Ext3nder-Installer"
 
-static int progressOld;
+// fix old iOS for new curl.a
+extern "C" int __isOSVersionAtLeast(int major, int minor, int patch)
+{
+	NSOperatingSystemVersion version;
+	version.majorVersion = major;
+	version.minorVersion = minor;
+	version.patchVersion = patch;
+	return [[NSProcessInfo processInfo] isOperatingSystemAtLeastVersion:version];
+}
+
+static unsigned int progressOld = 100;
 static void fragmentzip_callback(unsigned int progress)
 {
-    if(progressOld != progress) {
+    if(progressOld != progress && ((progress % 5)==0)) {
 		printf("Getting: %d%s\n",(int)progress, "%");
 		progressOld = progress;
 	}
 }
 
-static void rm_tmp_dir(void)
+static void rm_tmp_dir()
 {
 	printf("Cleaning Temp Files...\n");
 	printf("\n");
 	system("rm -rf "tmpDir);
+}
+
+static void resign_spawn(const char* ent, const char* path)
+{
+	pid_t pd;
+	char entArg[200];
+	strcpy(entArg, "-S");
+	strcat(entArg, ent);
+	const char* args[] = {"ldid", entArg, path, NULL};
+	posix_spawnattr_t attr;
+	posix_spawnattr_init(&attr);
+	posix_spawnattr_setflags(&attr, POSIX_SPAWN_START_SUSPENDED);
+	int rv = posix_spawn(&pd, "/usr/bin/ldid", NULL, &attr, (char **)&args, NULL);
+	kill(pd, SIGCONT);
+	if (!rv) {
+		int a;
+        waitpid(pd, &a, 0);
+    }
 }
 
 int main()
@@ -36,6 +63,12 @@ int main()
 	printf("** Cleaning \"Signed\" Folder From Ext3nder Documents.\n");
 	system("rm -rf //var/mobile/Documents/Ext3nder/Signed/");
 	
+	/*if([[[[NSFileManager defaultManager] attributesOfFileSystemForPath:@"/tmp" error:NULL]?:@{} objectForKey:NSFileSystemFreeSize]?:@(0) unsignedIntValue] < 20000000) {
+		printf("\n");
+		printf("** Requires 20MB free space in /tmp.\n");
+		goto error;
+	}*/
+	
 	printf("\n");
 	printf("** Fix Permissions Ext3nder Documents.\n");
 	system("mkdir -p //var/mobile/Documents/Ext3nder/");
@@ -44,7 +77,8 @@ int main()
 	
 	printf("\n");
 	printf("** Resetting Ext3nder Keychain.\n");
-	NSArray *secItemClasses = @[(__bridge id)kSecClassGenericPassword,
+	NSArray *secItemClasses;
+	secItemClasses = @[(__bridge id)kSecClassGenericPassword,
                        (__bridge id)kSecClassInternetPassword,
                        (__bridge id)kSecClassCertificate,
                        (__bridge id)kSecClassKey,
@@ -61,8 +95,10 @@ int main()
 	
 	printf("%s %s\n", "Downloading Latest Cydia Impactor", "...");
 	printf("%s %s\n", "https://cydia.saurik.com/api/latest/2 for", "/Impactor.dat");
-	fragmentzip_t *tt = fragmentzip_open("https://cydia.saurik.com/api/latest/2");
-    int rt = fragmentzip_download_file(tt, "Impactor.dat", tmpDir"/Impactor.dat", fragmentzip_callback);
+	fragmentzip_t *tt;
+    int rt;
+	tt = fragmentzip_open("https://cydia.saurik.com/api/latest/2");
+	rt = fragmentzip_download_file(tt, "Impactor.dat", tmpDir"/Impactor.dat", fragmentzip_callback);
     fragmentzip_close(tt);
 	printf("%s %s\n",rt==0?"Downloaded":"Failed", "/Impactor.dat");
 	printf("\n");
@@ -82,7 +118,9 @@ int main()
 	system("rm -rf "tmpDir"/Impactor.dat");
 	
 	printf("%s %s\n", "Extracting ", "extender.ipa");
-	system("cd "tmpDir";unzip -q extender.ipa");
+	if(system("cd "tmpDir";unzip -q extender.ipa")) {
+		goto error;
+	}
 	printf("%s %s\n", "Extracted", "extender.ipa");
 	printf("\n");
 	system("rm -rf "tmpDir"/extender.ipa");
@@ -96,7 +134,7 @@ int main()
 	
 	printf("- Copying Files.\n");
 	system("mv -f //var/Ext3nder-Installer/* "tmpDir"/Payload/Extender.app");
-	
+	system("rm -rf //var/Ext3nder-Installer");
 	
 	NSMutableDictionary *dictInfo;
 	dictInfo = [NSMutableDictionary dictionaryWithContentsOfFile:@tmpDir"/Payload/Extender.app/Info.plist"];
@@ -142,8 +180,9 @@ int main()
 	@"UTTypeDescription":@"public.archive",
 	@"UTTypeIdentifier":@"com.cydia.Extender.IPA",
 	@"UTTypeTagSpecification":@{@"public.filename-extension":@"ipa", @"public.mime-type":@"application/octet-stream"},}];
-    [dictInfo writeToFile:@tmpDir"/Payload/Extender.app/Info.plist" atomically:YES];
-	
+	if(![dictInfo writeToFile:@tmpDir"/Payload/Extender.app/Info.plist" atomically:YES]) {
+		goto error;
+	}
 	
 	
 	system("sed -i 's#/System/Library/Frameworks/Security\\.framework/Security#@executable_path/////////////////////////////Sys\\.dylib#g' "tmpDir"/Payload/Extender.app/Extender");
@@ -151,24 +190,37 @@ int main()
 	system("sed -i 's#ldid/ldid\\.cpp(498): _assert(stream\\.sputn(static_cast<const char \\*>(data) + to#=======\\*=======\\*=======CSSTASHEDAPPEXECUTABLESIGNATURE=======\\*=======\\*=======#g' "tmpDir"/Payload/Extender.app/Extender");
 	
 	
+	
 	printf("- Resigning & Applying Entitlements.\n");
-	system("ldid -S"tmpDir"/Payload/Extender.app/en.plist "tmpDir"/Payload/Extender.app/Extender");
-	system("ldid -S"tmpDir"/Payload/Extender.app/en.plist "tmpDir"/Payload/Extender.app/Extender.dylib");
+	resign_spawn(tmpDir"/Payload/Extender.app/en.plist", tmpDir"/Payload/Extender.app/Extender");
+	resign_spawn(tmpDir"/Payload/Extender.app/en.plist", tmpDir"/Payload/Extender.app/Extender.dylib");
 	
 	printf("\n");
 	printf("** Copying Cydia Extender To /Applications\n");
 	system("rm -rf //Applications/Ext3nder.app");
+	
+	/*if([[[[NSFileManager defaultManager] attributesOfFileSystemForPath:@"/Applications" error:NULL]?:@{} objectForKey:NSFileSystemFreeSize]?:@(0) unsignedIntValue] < 7000000) {
+		printf("\n");
+		printf("** Requires 7MB free space in /Applications.\n");
+		goto error;
+	}*/
+	
+	
 	system("chown -R 0:0 "tmpDir"/Payload/Extender.app");
 	system("chmod -R 755 "tmpDir"/Payload/Extender.app");
 	system("mv -f "tmpDir"/Payload/Extender.app //Applications/Ext3nder.app");
 	
-	
-	
 	rm_tmp_dir();
 	
 	printf("\n");
+	printf("** Setting '127.0.0.1 ocsp.apple.com' to /etc/hosts.\n");
+	system("sed -i -e '/ocsp.apple.com/d' /etc/hosts");
+	system("echo '127.0.0.1 ocsp.apple.com' >> /etc/hosts");
+	
+	
+	printf("\n");
 	printf("** Running uicache.\n");
-	system("su mobile -c uicache");
+	system("su mobile -c uicache &>/dev/null");
 	
 	printf("\n");
 	printf("** Respring your device!!!\n");
